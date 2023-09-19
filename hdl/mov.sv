6ec0ba04fed3bca17d06016ce5c3f6802e6d228d
Jörn Hoffmann <j.hoffmann@bitaggregat.de>
2023-09-19 16:06:50 +0200

    Remove unused parameter

--- hdl/mov.sv
+++ hdl/mov.sv
@@ -20,8 +20,7 @@
 import secv_pkg::*;
 
 module mov #(
-    parameter int XLEN = secv_pkg::XLEN,
-    parameter int ILEN = secv_pkg::ILEN
+    parameter int XLEN = secv_pkg::XLEN
 ) (
     // Control
     input   inst_t              inst_i,         // Instruction
