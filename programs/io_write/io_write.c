a473825fb8c57ac96797d8d128ce37e605fb708b
Jörn Hoffmann <j.hoffmann@bitaggregat.de>
2023-09-27 09:43:15 +0200

    Improve program
    - use picolibc
    - change linker script

--- programs/io_write/io_write.c
+++ programs/io_write/io_write.c
@@ -1,14 +1,9 @@
 #include <stdint.h>
 
-// Memory-mapped address for data memory
-volatile uint64_t *data_memory = (uint64_t *)0x10000000;
-
 int main() {
-    // Address and data to write
-    uint64_t address = 0x10000004;  // Example address in data memory
-    uint64_t value = 0xDEADBEEF;   // Example data to write
+    volatile uint64_t *mem = (uint64_t *)0x10000000;
+    uint64_t value         = 0xDEADBEEF;
 
-    // Write the value to the specified address
-    *data_memory = value;
+    *mem = value;
     return 0;
 }
