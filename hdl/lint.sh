909cf548f5cc7efb5f8374d2cc7f1d3ee891621a
Jörn Hoffmann <j.hoffmann@bitaggregat.de>
2023-09-28 10:27:10 +0200

    Rename mem_wb -> ram_wb

--- hdl/lint.sh
+++ hdl/lint.sh
@@ -3,7 +3,7 @@ VERILATOR=verilator
 VERIBLE=verible-verilog-lint
 SECV_PKG=secv_pkg.svh
 
-for file in alu.sv alu_core.sv alu_decoder.sv branch.sv decoder.sv gpr.sv mem_wb.sv mem.sv secv_pkg.svh secv.sv
+for file in alu.sv alu_core.sv alu_decoder.sv branch.sv decoder.sv gpr.sv mem.sv ram_wb.sv secv_pkg.svh secv.sv
 do
 	$VERILATOR --lint-only -I $SECV_PKG  $file
 	$VERIBLE $file --rules=line-length=length:120
