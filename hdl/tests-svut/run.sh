3143309f54f4abf351a4763cc9b16e03a0b91a71
Jörn Hoffmann <j.hoffmann@bitaggregat.de>
2023-09-28 09:18:12 +0200

    Improve run.sh
    - add selection of test

--- hdl/tests-svut/run.sh
+++ hdl/tests-svut/run.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 SVUT=$HOME/lib/svut/svutRun
 
-# Run specific tests
-$SVUT -test alu_core_testbench.sv
-$SVUT -test alu_decoder_testbench.sv
-$SVUT -test alu_testbench.sv
-$SVUT -test branch_testbench.sv
-$SVUT -test decoder_testbench.sv
-$SVUT -test gpr_testbench.sv
+# Check if arguments given
+if [ -z "$*" ]; then
+    tests=("alu_core" "alu_decoder" "alu" "branch" "decoder" "gpr")
+else
+    tests=$*
+fi
+
+# Run tests
+for name in $tests
+do
+    if [ "$name" == "alu_core" ]; then
+        $SVUT -test alu_core_testbench.sv
+
+    elif [ "$name" == "alu_decoder" ]; then
+        $SVUT -test alu_decoder_testbench.sv
+
+    elif [ "$name" == "alu" ]; then
+        $SVUT -test alu_testbench.sv
+
+    elif [ "$name" == "branch" ]; then
+        $SVUT -test branch_testbench.sv
+
+    elif [ "$name" == "decoder" ]; then
+        $SVUT -test decoder_testbench.sv
+
+    elif [ "$name" == "gpr" ]; then
+        $SVUT -test gpr_testbench.sv
+
+    else
+        echo "Test $name unknown"
+
+    fi
+done
