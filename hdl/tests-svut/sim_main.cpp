ac4f189e295aebabb8e23ed0c33ccbd3d22ba715
Jörn Hoffmann <joern@jBook.fritz.box>
2023-09-28 21:09:48 +0200

    Remove test in cpp file

--- hdl/tests-svut/sim_main.cpp
+++ hdl/tests-svut/sim_main.cpp
@@ -14,7 +14,6 @@
 #include "build/Vbranch_testbench.h"
 #include "build/Vdecode_testbench.h"
 #include "build/Vgpr_testbench.h"
-#include "build/Vram_2port_wb_testbench.h"
 #include "verilated.h"
 
 int main(int argc, char** argv, char** env) {
@@ -27,7 +26,6 @@ int main(int argc, char** argv, char** env) {
     Vbranch_testbench       *brn     = new Vbranch_testbench;
     Vdecode_testbench       *dec     = new Vdecode_testbench;
     Vgpr_testbench          *gpr     = new Vgpr_testbench;
-    Vram_2port_wb_testbench *ram2p   = new Vram_2port_wb_testbench;
 
     int timer = 0;
 
@@ -40,7 +38,6 @@ int main(int argc, char** argv, char** env) {
         brn->eval();
         dec->eval();
         gp->eval();
-        ram2p->eval();
     }
 
     // Final model cleanup
@@ -49,7 +46,6 @@ int main(int argc, char** argv, char** env) {
     brn->final();
     dec->final();
     gpr->final();
-    ram2p->final();
 
     // Destroy model
     delete alu;
@@ -57,7 +53,6 @@ int main(int argc, char** argv, char** env) {
     delete brn;
     delete dec;
     delete gpr;
-    delete ram2p;
 
     // Return good completion status
     return 0;
