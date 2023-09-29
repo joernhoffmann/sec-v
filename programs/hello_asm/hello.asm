343357f651328924937d97fdc53c6c97dc26b82b
Jörn Hoffmann <j.hoffmann@bitaggregat.de>
2023-09-29 15:14:56 +0200

    Add more elaborate "Tobias" example"

--- programs/hello_asm/hello.asm
+++ programs/hello_asm/hello.asm
@@ -5,10 +5,10 @@ main:
     addi    x1, x0, 0xca        # x1 = 0xcafe
     slli    x1, x1, 8
     addi    x1, x1, 0xfe
-    addi    x2, x0, 0xff        # x2 = 0xff
+    addi    x2, x0, 0xbe        # x2 = 0xbe
 
 loop:
-    sd      x1, 0(x2)           # M[0xff] = 0xcafe ++
+    sd      x1, 0xba(x2)           # M[0xff] = x1
     addi    x1, x1, 0x1         # x1++
     j       loop
 
