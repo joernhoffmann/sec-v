9893bbbd78b700b7d3853a1ffda3ab46a3dd2cc9
Jörn Hoffmann <joern@jBook.fritz.box>
2023-10-08 00:20:03 +0200

    Simplify alu_core_testbench
    - add task to test condition
    - use tast for checks

--- hdl/tests-svut/alu_core_testbench.sv
+++ hdl/tests-svut/alu_core_testbench.sv
@@ -56,6 +56,35 @@ module alu_core_testbench();
     end
     endtask
 
+    task tst;
+        input alu_op_t op;
+        input logic [XLEN-1 : 0] a;
+        input logic [XLEN-1 : 0] b;
+        input logic [XLEN-1 : 0] res;
+
+        op_i = op;
+        a_i = a;
+        b_i = b;
+        #1
+        `FAIL_IF_NOT_EQUAL(res_o, res);
+        `FAIL_IF(err_o);
+    endtask
+
+    task tst_fail;
+        input alu_op_t op;
+        input logic [XLEN-1 : 0] a;
+        input logic [XLEN-1 : 0] b;
+        input logic [XLEN-1 : 0] res;
+
+        op_i = op;
+        a_i = a;
+        b_i = b;
+        #1
+        `FAIL_IF_NOT_EQUAL(res_o, res);
+        `FAIL_IF_NOT(err_o);
+    endtask
+
+
     `TEST_SUITE("ALU_CORE")
     //  Available macros:"
     //
@@ -81,422 +110,201 @@ module alu_core_testbench();
     // Misc
     // ----------------------------------------------------------------------------------------------------------------
     `UNIT_TEST("NONE returns 0 and error")
-        a_i 	= 1;
-        b_i 	= 2;
-        op_i 	= ALU_OP_NONE;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF_NOT(err_o);
+        tst_fail(ALU_OP_NONE, 1, 2, 0);
     `UNIT_TEST_END
-
-/*  Currently all opcodes defined
-    `UNIT_TEST("Invalid opcode returns 0 and error")
-        a_i 	= 1;
-        b_i 	= 2;
-        op_i 	= alu_op_t'('b1);
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(!err_o);
-    `UNIT_TEST_END
-*/
 
     // ----------------------------------------------------------------------------------------------------------------
     // ADD - Addition
     // ----------------------------------------------------------------------------------------------------------------
     `UNIT_TEST("ADD zero")
-        a_i 	= 0;
-        b_i 	= 0;
-        op_i 	= ALU_OP_ADD;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADD, 0, 0, 0);
+        $display("out %x", res_o);
+        $display("err %x", err_o);
+
     `UNIT_TEST_END
 
     `UNIT_TEST("ADD positive integers")
-        a_i 	= 1;
-        b_i 	= 2;
-        op_i 	= ALU_OP_ADD;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 3);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADD, 1, 2, 3);
     `UNIT_TEST_END
 
     `UNIT_TEST("ADD positive and negative integer")
-        a_i 	= 1;
-        b_i 	= -3;
-        op_i 	= ALU_OP_ADD;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, -2);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADD, 1, -3, -2);
     `UNIT_TEST_END
 
     `UNIT_TEST("ADD with overflow")
-        a_i 	= ~0;
-        b_i 	= 1;
-        op_i 	= ALU_OP_ADD;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADD, ~0, 1, 0);
     `UNIT_TEST_END
 
     // ----------------------------------------------------------------------------------------------------------------
     // ADDW - Addition of 32-bit words
     // ----------------------------------------------------------------------------------------------------------------
     `UNIT_TEST("ADDW zero")
-        a_i 	= 0;
-        b_i 	= 0;
-        op_i 	= ALU_OP_ADDW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADDW, 0, 0, 0);
     `UNIT_TEST_END
 
     `UNIT_TEST("ADDW positive integers")
-        a_i 	= 1;
-        b_i 	= 2;
-        op_i 	= ALU_OP_ADDW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 3);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADDW, 1, 2, 3);
     `UNIT_TEST_END
 
     `UNIT_TEST("ADDW positive and negative integer")
-        a_i 	= 1;
-        b_i 	= -3;
-        op_i 	= ALU_OP_ADDW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, -2);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADDW, 1, -3, -2);
     `UNIT_TEST_END
 
     `UNIT_TEST("ADDW with overflow")
-        a_i 	= ~0;
-        b_i 	= 1;
-        op_i 	= ALU_OP_ADDW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADDW, ~0, 1, 0);
     `UNIT_TEST_END
 
     `UNIT_TEST("ADDW generates sign extend")
-        a_i 	= {32'b0, -32'd2};
-        b_i 	=  1;
-        op_i 	= ALU_OP_ADDW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, {XLEN{1'b1}});
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADDW, -2, 1, ~0);
     `UNIT_TEST_END
 
     `UNIT_TEST("ADDW overflows and stays in range")
-        a_i 	= {32'b0, {32{1'b1}}};
-        b_i 	=  128;
-        op_i 	= ALU_OP_ADDW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 128-1);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_ADDW, 64'hffff_ffff, 128, 128-1);
     `UNIT_TEST_END
 
     // ----------------------------------------------------------------------------------------------------------------
     // SUB - Substract
     // ----------------------------------------------------------------------------------------------------------------
     `UNIT_TEST("SUB with zero")
-        a_i 	= 0;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SUB;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SUB, 0, 0, 0);
     `UNIT_TEST_END
 
     `UNIT_TEST("SUB with positive integers")
-        a_i 	= 1;
-        b_i 	= 2;
-        op_i 	= ALU_OP_SUB;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, -1);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SUB, 1, 2, -1);
     `UNIT_TEST_END
 
     `UNIT_TEST("SUB with positive and negative number")
-        a_i 	= 1;
-        b_i 	= -3;
-        op_i 	= ALU_OP_SUB;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 4);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SUB, 1, -3, 4);
     `UNIT_TEST_END
 
     `UNIT_TEST("SUB with overflow")
-        a_i 	= ~0;
-        b_i 	= -1;
-        op_i 	= ALU_OP_SUB;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SUB, ~0, -1, 0);
     `UNIT_TEST_END
 
     `UNIT_TEST("SUB of maximum negative values")
-        a_i 	= ~0;
-        b_i 	= ~0;
-        op_i 	= ALU_OP_SUB;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SUB, ~0, ~0, 0);
     `UNIT_TEST_END
 
     // ----------------------------------------------------------------------------------------------------------------
     // SLL - Shift Left Logic
     // ----------------------------------------------------------------------------------------------------------------
     `UNIT_TEST("SLL shift-in no bits")
-        a_i 	= ~0;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, ~0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SLL, ~0, 0, ~0);
     `UNIT_TEST_END
 
     `UNIT_TEST("SLL shift-in 1-bit zero")
-        a_i 	= ~0;
-        b_i 	= 1;
-        op_i 	= ALU_OP_SLL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, ~0 << 1);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SLL, 64'hffffffff_ffffffff, 1, 64'hffffffff_fffffffe);
     `UNIT_TEST_END
 
     `UNIT_TEST("SLL shift-in 63-bit zeros")
-        a_i 	= ~0;
-        b_i 	= XLEN-1;
-        op_i 	= ALU_OP_SLL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, {1'b1, {XLEN-1{1'b0}}});
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SLL, 64'hffffffff_ffffffff, 63, 64'h80000000_00000000);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SLL shift-in 64-bits zeros is pot possible (shamt [5:0])")
-        a_i 	= ~0;
-        b_i 	= XLEN;
-        op_i 	= ALU_OP_SLL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, ~0);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SLL shift-left 65 is like shift-left 1 (shamt limited to [5:0])")
+        tst(ALU_OP_SLL, 64'hffffffff_ffffffff, 65, 64'hffffffff_fffffffe);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SLL shift-left pattern 16")
-        a_i 	= 'hcafe;
-        b_i 	= 16;
-        op_i 	= ALU_OP_SLL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 'hcafe0000);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SLL shift-left 16 some pattern")
+        tst(ALU_OP_SLL, 64'hcafe, 16, 64'hcafe0000);
     `UNIT_TEST_END
 
     `UNIT_TEST("SLL shift-left pattern 32 digits")
-        a_i 	= 'hcafebabe;
-        b_i 	= 32;
-        op_i 	= ALU_OP_SLL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 64'hcafebabe_00000000);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SLL, 64'hcafebabe, 32, 64'hcafebabe_00000000);
     `UNIT_TEST_END
 
     // ----------------------------------------------------------------------------------------------------------------
     // SRL - Shift Right Logic
     // ----------------------------------------------------------------------------------------------------------------
     `UNIT_TEST("SRL shift-out no bit")
-        a_i 	= ~0;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SRL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, ~0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SRL, ~0, 0, ~0);
     `UNIT_TEST_END
 
     `UNIT_TEST("SRL shift-out one bit")
-        a_i 	= ~0;
-        b_i 	= 1;
-        op_i 	= ALU_OP_SRL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, ~0 >> 1);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SRL, ~0, 1, ~0 >> 1);
     `UNIT_TEST_END
 
     `UNIT_TEST("SRL shift-out 63-bits")
-        a_i 	= ~0;
-        b_i 	= XLEN-1;
-        op_i 	= ALU_OP_SRL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 1);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SRL, ~0, 63, 1);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SRL shift-out 64-bits is pot possible (shamt [5:0])")
-        a_i 	= ~0;
-        b_i 	= XLEN;
-        op_i 	= ALU_OP_SRL;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, ~0);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SRL shift-right 65-bits is like shift-right 1-bit (shamt limited to [5:0])")
+        tst(ALU_OP_SRL, ~0, 65, ~0 >> 1);
     `UNIT_TEST_END
 
     // ----------------------------------------------------------------------------------------------------------------
     // SRA - Shift Right Arithmetic
     // ----------------------------------------------------------------------------------------------------------------
     `UNIT_TEST("SRA shift-in no bit")
-        a_i 	= 1 << XLEN-1;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SRA;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, { 1'b1, {XLEN-1{1'b0}}});
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SRA, 64'h80000000_00000000, 0, 64'h80000000_00000000);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SRA shift-in single bit")
-        a_i 	= 1 << XLEN-1;	    // Set MSB to 1
-        b_i 	= 1;
-        op_i 	= ALU_OP_SRA;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, { 2'b11, {XLEN-2{1'b0}}});
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SRA shift-in additional bit")
+        tst(ALU_OP_SRA, 64'h80000000_00000000, 1, 64'hc0000000_00000000);
     `UNIT_TEST_END
 
     `UNIT_TEST("SRA shift-in 63-Bit so that all bits set")
-        a_i 	= 1 << XLEN-1;	    // Set MSB to 1
-        b_i 	= XLEN-1;
-        op_i 	= ALU_OP_SRA;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, ~0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SRA, 64'h80000000_00000000, 63, 64'hffffffff_ffffffff);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SRA shift-in 64 bits not possible")
-        a_i 	= 1 << XLEN-1;
-        b_i 	= XLEN;
-        op_i 	= ALU_OP_SRA;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 1 << XLEN-1);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SRA shift-in 65 bits is like shift in one bit (shamt limited)")
+        tst(ALU_OP_SRA, 64'h80000000_00000000, 65, 64'hc0000000_00000000);
     `UNIT_TEST_END
 
     // ----------------------------------------------------------------------------------------------------------------
     // SLLW - Shift Left Logic Word
     // ----------------------------------------------------------------------------------------------------------------
     `UNIT_TEST("SLLW shift-in no bit")
-        a_i 	= 1;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLLW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 1);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SLLW, 1, 0, 1);
     `UNIT_TEST_END
 
     `UNIT_TEST("SLLW shift-in no bit and correctly sign extend")
-        a_i 	= 1 << WLEN-1;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLLW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 64'hffff_ffff_8000_0000);
-        `FAIL_IF(err_o);
-        $display("res_o : %x", res_o);
+        tst(ALU_OP_SLLW, 64'h0000_0000_8000_0000, 0, 64'hffff_ffff_8000_0000);
     `UNIT_TEST_END
 
     `UNIT_TEST("SLLW shift-in 31-bit zeros")
-        a_i 	= ~0;
-        b_i 	= WLEN-1;
-        op_i 	= ALU_OP_SLLW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 64'hffff_ffff_8000_0000);
-        `FAIL_IF(err_o);
-        $display("res_o : %x", res_o);
+        tst(ALU_OP_SLLW, 64'hffff_ffff_ffff_ffff, 31, 64'hffff_ffff_8000_0000);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SLLW shift-in 32-bit zeros not possible")
-        a_i 	= ~0;
-        b_i 	= WLEN;
-        op_i 	= ALU_OP_SLLW;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, ~0);
-        `FAIL_IF(err_o);
-        $display("res_o : %x", res_o);
+    `UNIT_TEST("SLLW shift-in 33-bit zeros is like 1-bit (shamt limited)")
+        tst(ALU_OP_SLLW, 64'hffff_ffff_ffff_ffff, 32, 64'hffff_ffff_ffff_ffff);
     `UNIT_TEST_END
 
     // -----------------------------------------------------------------------------------------------------------------
     // SLT - Set Less Than
     // -----------------------------------------------------------------------------------------------------------------
-    `UNIT_TEST("SLT check same")
-        a_i 	= 0;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLT;
-        #1 `FAIL_IF_NOT_EQUAL(res_o, 0);
+    `UNIT_TEST("SLT check same results to 0")
+        tst(ALU_OP_SLT, 0, 0, 0);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SLT check less than")
-        a_i 	= -1;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLT;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 1);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SLT check less than results to 1")
+        tst(ALU_OP_SLT, -1, 0, 1);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SLT check larger")
-        a_i 	= 2;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLT;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SLT check larger results to 0")
+        tst(ALU_OP_SLT, 2, 1, 0);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SLT check less than both neg")
-        a_i 	= 1 << XLEN-1;
-        b_i 	= 1 << XLEN-1 | 1 << XLEN-2;
-        op_i 	= ALU_OP_SLT;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 1);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SLT check less than and both negative results to 1")
+        tst(ALU_OP_SLT, 1 << XLEN-1, 1 << XLEN-1 | 1 << XLEN-2, 1);
     `UNIT_TEST_END
 
     // -----------------------------------------------------------------------------------------------------------------
     // SLTU - Set Less Than Unsigned
     // -----------------------------------------------------------------------------------------------------------------
-    `UNIT_TEST("SLTU check same")
-        a_i 	= 0;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLTU;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SLTU check same results to 0")
+        tst(ALU_OP_SLTU, 0, 0, 0);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SLTU check less than")
-        a_i 	= 0;
-        b_i 	= 2;
-        op_i 	= ALU_OP_SLTU;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 1);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SLTU check less than results to 1")
+        tst(ALU_OP_SLTU, 0, 2, 1);
     `UNIT_TEST_END
 
-    `UNIT_TEST("SLTU check larger")
-        a_i 	= 128;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLTU;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+    `UNIT_TEST("SLTU check larger results to 0")
+        tst(ALU_OP_SLTU, 128, 0, 0);
     `UNIT_TEST_END
 
     `UNIT_TEST("SLTU check not using signed negative")
-        a_i 	= -1;
-        b_i 	= 0;
-        op_i 	= ALU_OP_SLTU;
-        #1
-        `FAIL_IF_NOT_EQUAL(res_o, 0);
-        `FAIL_IF(err_o);
+        tst(ALU_OP_SLTU, ~0, 0, 0);
     `UNIT_TEST_END
-
     `TEST_SUITE_END
-
 endmodule
