f2fd9bf6e904a0a1bf9e8c82eff7b296a550abfa
Till Mahlburg <mahlburg@posteo.de>
2023-10-10 17:08:23 +0200

    some fixes, not ready yet

--- hdl/memtag.sv
+++ hdl/memtag.sv
@@ -7,7 +7,9 @@
  * Purpose  : Memory Tagging unit for the SEC-V processor.
  *
  * TODO:
- * - exception interrupt
+ * - output as funit_out_t
+ * - use opcode instead of set_i
+ * - better use of mem_adr vs fu_i.src1
  *
  * History
  *  v1.0    - Initial version
@@ -18,13 +20,13 @@ import secv_pkg::*;
 
 module memtag #(
     /* size of tags in bit */
-    parameter int TAG_SIZE = 32,
+    parameter int TLEN = 16,
     /* size of granules in byte */
     parameter int GRANULARITY = 8,
     /* address size in bit */
     parameter int ADR_WIDTH = 8,
     /* tag memory address width in bit */
-    parameter int TADR_WIDTH = 32,
+    parameter int TADR_WIDTH = 16,
     parameter int TSEL_WIDTH = 1
 ) (
     input funit_in_t fu_i,
@@ -32,7 +34,7 @@ module memtag #(
     input logic set_i,
 
     output logic err_o,
-    output logic [ADR_WIDTH-1 : 0] err_adr,
+    output logic [ADR_WIDTH-1 : 0] err_adr_o,
 
     /* tag memory */
     output logic                    tmem_cyc_o,
@@ -40,15 +42,15 @@ module memtag #(
     output logic [TSEL_WIDTH-1 : 0] tmem_sel_o,
     output logic [TADR_WIDTH-1 : 0] tmem_adr_o,
     output logic                    tmem_we_o,
-    output logic [  TAG_SIZE-1 : 0] tmem_dat_o,
-    input  logic [  TAG_SIZE-1 : 0] tmem_dat_i,
+    output logic [TLEN-1 : 0]       tmem_dat_o,
+    input  logic [TLEN-1 : 0]       tmem_dat_i,
     input  logic                    tmem_ack_i
 );
-  logic [ADR_WIDTH-1 : 0] mem_addr;
+  logic [ADR_WIDTH-1 : 0] mem_adr;
   assign mem_adr = ADR_WIDTH'(fu_i.src1);
 
-  logic [TAG_SIZE-1 : 0] tag;
-  assign tag = mem_adr[ADR_WIDTH-1 : ADR_WIDTH-1-TAG_SIZE];
+  logic [TLEN-1 : 0] tag;
+  assign tag = fu_i.src1[XLEN-1 : XLEN-TLEN];
 
   logic set;
   assign set = set_i;
@@ -73,16 +75,16 @@ module memtag #(
     if (fu_i.ena) begin
       tmem_cyc_o = 1'b1;
       tmem_stb_o = 1'b1;
-      tmem_adr_o = mem_adr[ADR_WIDTH-1-TAG_SIZE : 0] / GRANULARITY;
+      tmem_adr_o = mem_adr / GRANULARITY;
 
-      if (set) begin
+      if (fu_i.op == OPCODE_CUSTOM_0) begin
         /* set tag in tag memory */
         tmem_dat_o = tag;
         tmem_sel_o = 'b1;
         tmem_we_o  = 'b1;
       end else begin
         /* compare tag with tag memory */
-        tmem_sel = 1'b1;
+        tmem_sel_o = 1'b1;
         if (tmem_dat_i != tag) begin
           err = 1'b1;
           err_adr = mem_adr;
