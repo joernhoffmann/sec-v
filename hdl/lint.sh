b72fc23cf9a217069efcdb4de3b2c8c8325b1467
Till Mahlburg <mahlburg@posteo.de>
2023-10-13 17:14:17 +0200

    lint mtag_decoder.sv

--- hdl/lint.sh
+++ hdl/lint.sh
@@ -4,7 +4,7 @@ VERIBLE=verible-verilog-lint
 SECV_PKG=secv_pkg.svh
 
 for file in alu.sv alu_core.sv alu_decoder.sv branch.sv decoder.sv gpr.sv \
-		    mem.sv memtag.sv ram_wb.sv rom_wb.sv secv_pkg.svh secv.sv
+		    mem.sv mtag.sv mtag_decoder.sv ram_wb.sv rom_wb.sv secv_pkg.svh secv.sv
 do
 	$VERILATOR --lint-only -I $SECV_PKG  $file
 	$VERIBLE $file --rules=line-length=length:120
