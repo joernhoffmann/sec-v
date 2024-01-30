351830418a4ba188e18ec0cb262c20c52e392488
Till Mahlburg <mahlburg@posteo.de>
2024-01-30 12:30:59 +0100

    update lint.sh to check mtag_mem.sv

--- hdl/lint.sh
+++ hdl/lint.sh
@@ -4,10 +4,10 @@ VERIBLE=verible-verilog-lint
 SECV_PKG=secv_pkg.svh
 
 for file in alu.sv alu_core.sv alu_decoder.sv branch.sv decoder.sv gpr.sv \
-		    mem.sv mtag.sv mtag_chk.sv mtag_decoder.sv csr.sv csr_regs.sv \
-		    ram_wb.sv rom_wb.sv secv_pkg.svh csr_pkg.svh secv.sv
+            mem.sv mtag.sv mtag_chk.sv mtag_decoder.sv mtag_mem.sv csr.sv \
+            csr_regs.sv ram_wb.sv rom_wb.sv secv_pkg.svh csr_pkg.svh secv.sv
 do
-	$VERILATOR --lint-only -I $SECV_PKG $file
-	$VERIBLE $file --rules=line-length=length:120
+    $VERILATOR --lint-only -I $SECV_PKG $file
+    $VERIBLE $file --rules=line-length=length:120
 done
 
